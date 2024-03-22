0ee7d3d7ae110906f1184218c0f2041b8453399d
Fazlı Altun <32793348+fazlialtunn@users.noreply.github.com>
2024-03-22 11:29:03 +0300

    Initial Commit

---
+++ NetflixCloneMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixCloneMVVM
+//
+//  Created by Fazli Altun on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
